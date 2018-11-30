MegTron/megtron-extension

Connect gas price chart to gas station api.

ui/lib/local-storage-helpers.js
@@ -1,0 +1,20 @@
+export function loadLocalStorageData (itemKey) {
+  try {
+    const serializedData = localStorage.getItem(itemKey)
+    if (serializedData === null) {
+      return undefined
+    }
+    return JSON.parse(serializedData)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export function saveLocalStorageData (data, itemKey) {
+  try {
+    const serializedData = JSON.stringify(data)
+    localStorage.setItem(itemKey, serializedData)
+  } catch (err) {
+    console.log(err)
+  }
+}

whitecandylee/metamask-extension

ui/lib/local-storage-helpers.js
@@ -1,0 +1,20 @@
+export function loadLocalStorageData (itemKey) {
+  try {
+    const serializedData = localStorage.getItem(itemKey)
+    if (serializedData === null) {
+      return undefined
+    }
+    return JSON.parse(serializedData)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export function saveLocalStorageData (data, itemKey) {
+  try {
+    const serializedData = JSON.stringify(data)
+    localStorage.setItem(itemKey, serializedData)
+  } catch (err) {
+    console.warn(err)
+  }
+}